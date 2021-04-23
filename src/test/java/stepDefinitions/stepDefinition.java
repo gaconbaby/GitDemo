77b01f8605236be1f2e99946cb8c8d6c3b84b9cf
gaconbaby <nguyenthilanhuong379@gmail.com>
2021-04-23 17:35:59 +0700

    this is commit from GitDemo

--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -14,6 +14,7 @@ public class stepDefinition {
     @Given("^User is on NetBanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
         System.out.println("User is on NetBanking landing page");
+        System.out.println("Cards are displayed");
 //        throw new PendingException();
 
     }
@@ -21,17 +22,20 @@ public class stepDefinition {
     @When("^User login into application with username and password$")
     public void user_login_into_application_with_username_and_password() throws Throwable {
         System.out.println("User login into application with username and password");
+        System.out.println("Cards are displayed");
 //        throw new PendingException();
     }
 
     @Then("^Home page is populated$")
     public void home_page_is_populated() throws Throwable {
         System.out.println("Home page is populated");
+        System.out.println("Cards are displayed");
 //        throw new PendingException();
     }
 
     @And("^Cards are displayed$")
     public void cards_are_displayed() throws Throwable {
+        System.out.println("Cards are displayed");
         System.out.println("Cards are displayed");
 //        throw new PendingException();
     }
